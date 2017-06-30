1fc12d23a4490476261c649a191b6b8d9b16cec2
Nicholas Clawson <nickclaw@gmail.com>
2017-06-30 00:21:46 -0700

    Add some more blame tests

--- test/blame.test.js
+++ test/blame.test.js
@@ -19,13 +19,55 @@ async function getError(fn) {
 
 describe('blame', () => {
 
-  it('should work', async () => {
+  it('should work with an expression', async () => {
     const text = `<?= (function test(){ (function(){foo})() })() ?>`;
+
     const ast = parse(text);
     const code = build(ast);
     const err = await getError(() => execute(code, filename, module));
     const newErr = await getError(() => blame(code, filename, err));
 
     expect(newErr.stack).to.contain('/path/to/foo.md:1');
+  });
+
+  it('should work with a code block', async () => {
+    const text = stripIndent`
+      <?doc
+        const a = 1;
+        c.map(() => {});
+      ?>
+    `;
+
+    const ast = parse(text);
+    const code = build(ast);
+    const err = await getError(() => execute(code, filename, module));
+    const newErr = await getError(() => blame(code, filename, err));
+
+    expect(newErr.stack).to.contain('/path/to/foo.md:3');
+  });
+
+  it('should work with nested functions', async () => {
+    const text = stripIndent`
+      <?doc
+        const willThrow = () => { foo };
+        const willCall = () => willThrow();
+      ?>
+
+      some random text
+
+      <?= willCall() ?>
+    `;
+
+    const ast = parse(text);
+    const code = build(ast);
+    const err = await getError(() => execute(code, filename, module));
+    const newErr = await getError(() => blame(code, filename, err));
+
+const expected = `ReferenceError: foo is not defined
+    at willThrow (/path/to/foo.md:2)
+    at willCall (/path/to/foo.md:3)
+    at $fn (/path/to/foo.md:8)`
+
+    expect(newErr.stack).to.equal(expected);
   });
 });
